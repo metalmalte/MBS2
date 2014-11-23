4fed97b7281e876bcd89bbadc15d0d28f6abe976
Malte Fentroß <m.fentross@app-buddy.de>
2014-11-23 16:20:39 +0100

    implemented 4

---
+++ obj/MIPShiftScale.d
@@ -0,0 +1,10 @@
+obj/MIPShiftScale.d: src/MIPShiftScale.cpp src/Image.h src/ImageIO.h \
+  src/ShiftScaleFilter.h src/ImageFilter.h
+
+src/Image.h:
+
+src/ImageIO.h:
+
+src/ShiftScaleFilter.h:
+
+src/ImageFilter.h:
